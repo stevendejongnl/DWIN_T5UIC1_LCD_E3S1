0beb0476d6cc1e1548e545675c84fe4b9adc16a0
🍟 <steven@steven-dejong.nl>
2023-11-17 13:30:00 +0100

    Use subprocess

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-import os
+import subprocess
 
 from dwinlcd import DWIN_LCD
 
 encoder_Pins = (19, 26)
 button_Pin = 13
 LCD_COM_Port = '/dev/ttyAMA0'
-API_Key, _ = os.system('/home/steven/moonraker/scripts/fetch-apikey.sh')
+API_Key = subprocess.check_output('/home/steven/moonraker/scripts/fetch-apikey.sh', shell=True, text=True)
 
 DWINLCD = DWIN_LCD(
     LCD_COM_Port,
     encoder_Pins,
     button_Pin,
-    API_Key
+    API_Key.strip()
 )
