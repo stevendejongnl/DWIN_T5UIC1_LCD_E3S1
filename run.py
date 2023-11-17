0b238e83ea8722eb18c7e06730d03b4ff13496f2
🍟 <steven@steven-dejong.nl>
2023-11-17 13:23:13 +0100

    Can get api key, try to run

--- run.py
+++ run.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 import os
+
 from dwinlcd import DWIN_LCD
 
 encoder_Pins = (19, 26)
 button_Pin = 13
 LCD_COM_Port = '/dev/ttyAMA0'
-API_Key = os.system('/home/steven/moonraker/scripts/fetch-apikey.sh')
+API_Key, _ = os.system('/home/steven/moonraker/scripts/fetch-apikey.sh')
 
-from pprint import pprint
-pprint(API_Key)
-
-# DWINLCD = DWIN_LCD(
-#         LCD_COM_Port,
-#         encoder_Pins,
-#         button_Pin,
-#         API_Key
-# )
+DWINLCD = DWIN_LCD(
+    LCD_COM_Port,
+    encoder_Pins,
+    button_Pin,
+    API_Key
+)
