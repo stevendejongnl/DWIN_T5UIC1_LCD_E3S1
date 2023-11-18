1719da1f47f88165deee505fcfa2a47de3d797c2
🍟 <steven@steven-dejong.nl>
2023-11-18 14:10:23 +0100

    Add print for start time

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import subprocess
+import datetime
 
 from dwinlcd import DWIN_LCD
 
@@ -7,6 +8,8 @@ encoder_Pins = (19, 26)
 button_Pin = 13
 LCD_COM_Port = '/dev/ttyAMA0'
 API_Key = subprocess.check_output('/home/steven/moonraker/scripts/fetch-apikey.sh', shell=True, text=True)
+
+print(f"Run start time: {datetime.datetime.now():%Y-%m-%d %H:%M}")
 
 DWINLCD = DWIN_LCD(
     LCD_COM_Port,
