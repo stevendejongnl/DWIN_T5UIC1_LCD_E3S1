35cb17fd1273aa5af9abd40500697670c67604f1
🍟 <steven@steven-dejong.nl>
2023-11-18 14:24:03 +0100

    Use Popen

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-import subprocess
 import datetime
+import subprocess
+from subprocess import PIPE
 
 from dwinlcd import DWIN_LCD
+
+process = subprocess.Popen('/home/steven/moonraker/scripts/fetch-apikey.sh', stdout=PIPE, stderr=PIPE)
+output, error = process.communicate()
+print(output)
+API_Key = output
+process.kill()
 
 encoder_Pins = (19, 26)
 button_Pin = 13
 LCD_COM_Port = '/dev/ttyAMA0'
-API_Key = subprocess.check_output('/home/steven/moonraker/scripts/fetch-apikey.sh', shell=True, text=True)
 
 print(f"Run start time: {datetime.datetime.now():%Y-%m-%d %H:%M}")
 
